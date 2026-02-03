fe838f61dcac93c8de870f1ca26ffaa1f2c3d8e0
leonardo <leonardogaspar2001@gmail.com>
2026-02-03 16:18:31 -0300

    feat(domain): Professional entity and repository

---
+++ src/main/java/com/leogaspar/appointment/domain/model/Professional.java
@@ -0,0 +1,94 @@
+package com.leogaspar.appointment.domain.model;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "professional")
+public class Professional {
+	
+	@Id
+	private String id;
+	
+	private String name;
+	private String specialty;
+	private String schedule;
+	LocalDateTime createdAt;
+	
+	public Professional() {
+		
+	}
+
+	public Professional(String id, String name, String specialty, String schedule, LocalDateTime createdAt) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.specialty = specialty;
+		this.schedule = schedule;
+		this.createdAt = createdAt;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSpecialty() {
+		return specialty;
+	}
+
+	public void setSpecialty(String specialty) {
+		this.specialty = specialty;
+	}
+
+	public String getSchedule() {
+		return schedule;
+	}
+
+	public void setSchedule(String schedule) {
+		this.schedule = schedule;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Professional other = (Professional) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+	
+}
