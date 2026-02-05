feec050a7af990a3842668e707a9f283b4e03c67
leonardo <leonardogaspar2001@gmail.com>
2026-02-05 18:17:15 -0300

    feat(resource): implement find professional by id

--- src/main/java/com/leogaspar/appointment/web/controller/ProfessionalController.java
+++ src/main/java/com/leogaspar/appointment/web/controller/ProfessionalController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,12 +36,19 @@ public class ProfessionalController {
 	}
 	
 	@GetMapping
-	public ResponseEntity<List<Professional>> getAllProfessional() {
+	public ResponseEntity<List<Professional>> getAllProfessionals() {
 		
 		List<Professional> professionals = repository.findAll(); 
 		
 		
 		return ResponseEntity.ok().body(professionals);
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Professional> getProfessionalById(@PathVariable String id) {
+		
+		return  repository.findById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+	}
+	
 
 }
