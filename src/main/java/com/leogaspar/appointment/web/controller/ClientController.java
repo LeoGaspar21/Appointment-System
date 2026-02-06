a19ac5e2b2abfe942e61b7a2a806ac83cd2725f2
leonardo <leonardogaspar2001@gmail.com>
2026-02-05 23:32:07 -0300

    feat(resource): implement find Client by id

--- src/main/java/com/leogaspar/appointment/web/controller/ClientController.java
+++ src/main/java/com/leogaspar/appointment/web/controller/ClientController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -38,6 +39,11 @@ public class ClientController {
 		
 		return ResponseEntity.ok().body(clients);
 		
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Client> getClientById(@PathVariable String id) {
+		return repository.findById(id).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
 	}
 
 }
